44eedb45800cde81336d41ba4b1d12736cf8f8b5
Daniel Bauer <bauer@bioinfo4.compbiol.bio.tu-darmstadt.de>
2017-12-12 14:50:33 +0100

    update

--- polybar/spotify.py
+++ polybar/spotify.py
@@ -6,7 +6,11 @@ try:
     spotify = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
     spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
     props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-    print(str(props['xesam:artist'][0]) + " - " + str(props['xesam:title']))
+    try:
+        out = str(props['xesam:artist'][0]) + " - " + str(props['xesam:title'])
+    except IndexError:
+        out = ''
+    print(out)
     exit
 except dbus.exceptions.DBusException:
     exit
