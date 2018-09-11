d4c3f94514f44913f50704ae24f7ecb4f44a7564
Daniel Bauer <bauer@cbs.tu-darmstadt.de>
2018-09-11 15:53:13 +0200

    polybar

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -8,6 +8,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 polybar -c ~/.config/polybar/config example_left &
 polybar -c ~/.config/polybar/config example_right & 
-polybar -c ~/.config/polybar/config example_laptop &
 
 echo "Bars launched..."
