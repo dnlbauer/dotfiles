82ba97ca474273c28c75ad062e2a52d4c9b515ee
Daniel Bauer <bauer@cbs.tu-darmstadt.de>
2019-03-18 13:31:22 +0100

    matplotlib

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -2,11 +2,14 @@
 dir="${HOME}/dotfiles"
 conffolder="${HOME}/.config"
 
-dotfiles=("matplotlibrc" ".pymolrc" ".pymol" ".vimrc" ".vim" ".vimrc" ".zshrc" ".zshrc_functions" ".zim" ".zimrc" ".gitconfig")
+dotfiles=(".pymolrc" ".pymol" ".vimrc" ".vim" ".vimrc" ".zshrc" ".zshrc_functions" ".zim" ".zimrc" ".gitconfig")
 for dotfile in "${dotfiles[@]}"; do
 	ln -sf $dir/$dotfile ${HOME}/${dotfile}
 done
 
 # fonts
-mkdir -p .config/fontconfig
+mkdir -p ~/.config/fontconfig
 ln -sf $dir/.config/fontconfig/fonts.conf $conffolder/fontconfig/fonts.conf
+
+# matplotlib
+ln -sf $dir/.config/matplotlib ~/.config/matplotlib
