ab0f7fbec8cd698e97a5bd165fd9eb6a50044c1e
Daniel Bauer <bauer@cbs.tu-darmstadt.de>
2019-03-23 16:09:51 +0100

    install zim with bootstrapping script

--- install.sh
+++ install.sh
@@ -6,5 +6,28 @@ if [ `cat /etc/passwd | grep $USER | awk -F ':' '{print $7}'` != "/bin/zsh" ]; t
 	chsh -s $(which zsh)
 fi
 
+# ZIM Installation
+if [ ! -d "$HOME/.zim" ]; then
+	echo "Installing ZIM"
+	git clone --recursive --depth=1 https://github.com/zimfw/zimfw.git \
+		${ZDOTDIR:-${HOME}}/.zim
+else
+	echo "ZIM already exists."
+fi
+
+# Powerlevel9k Theme
+if [ ! -d "$HOME/.zim/modules/prompt/external-themes/powerlevel9k" ]; then
+	echo "Installing Powerlevel9k theme"
+	git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git \
+		~/.zim/modules/prompt/external-themes/powerlevel9k
+	ln -s ~/.zim/modules/prompt/external-themes/powerlevel9k/powerlevel9k.zsh-theme ~/.zim/modules/prompt/functions/prompt_powerlevel9k_setup
+else
+	echo "Powerlevel9k already exists."
+fi
+
 # linking base stuff
 $HOME/.dotfiles/dotfiles.sh link base  
+
+# TODO install anaconda
+# TODO install fasd
+# TODO install marker
