88baca7458cb46b140ce1dea1bd4ee47231dab8f
danijoo <danijoo@users.noreply.github.com>
2020-09-25 10:01:02 +0200

    stuff

--- profiles/laptop/.config/polybar/polybar.sh
+++ profiles/laptop/.config/polybar/polybar.sh
@@ -5,8 +5,19 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config laptop &
-polybar -c ~/.config/polybar/config laptop_extern &
+if type "xrandr"; then
+  # primary screen
+  for m in $(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload laptop &
+  done
+  # other screens
+  for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload laptop_secondary &
+  done
+else
+  polybar --reload laptop &
+fi
+# polybar -c ~/.config/polybar/config laptop &
+# polybar -c ~/.config/polybar/config laptop_extern &
 
 echo "Bars launched..."
