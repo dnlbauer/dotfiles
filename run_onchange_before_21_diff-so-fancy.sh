da24879fd634a42daa568aac54a84d3ba1919107
Daniel Bauer <github@dbauer.me>
2025-08-03 14:42:56 +0200

    fix diff so fancy install

--- run_onchange_before_21_diff-so-fancy.sh
+++ run_onchange_before_21_diff-so-fancy.sh
@@ -3,6 +3,7 @@
 if [ ! -f "$HOME/.local/bin/diff-so-fancy" ]; then
     echo "Installing diff-so-fance into .local/bin"
     if [ ! -d "$HOME/.local/lib/diff-so-fancy" ]; then
+        mkdir -p $HOME/.local/lib
         git clone --depth 1 https://github.com/so-fancy/diff-so-fancy.git $HOME/.local/lib/diff-so-fancy
     fi
     ln -s $HOME/.local/lib/diff-so-fancy/diff-so-fancy $HOME/.local/bin/diff-so-fancy
