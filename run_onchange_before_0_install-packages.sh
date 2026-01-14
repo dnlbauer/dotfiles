02b86c2d2833034b06e42d5fa0c2c5df39d22ad3
Daniel Bauer <daniel.bauer@senckenberg.de>
2026-01-14 11:05:21 +0100

    feat: install packages prints output

--- run_onchange_before_0_install-packages.sh
+++ run_onchange_before_0_install-packages.sh
@@ -3,13 +3,14 @@
 set -e
 
 echo "Installing packages"
+echo "Updating sources..."
 sudo apt update -qq
 
 for package in xsel git git-extras neovim git ripgrep; do
     if dpkg -s "$package" >/dev/null 2>&1; then
         echo $package is already installed.
     else
-        echo Installing $package
+        echo Installing $package ...
         sudo apt install -y $package
     fi
 done
